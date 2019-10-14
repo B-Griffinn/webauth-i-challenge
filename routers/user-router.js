0cc161563b6a6a0cbd571993d5495034591236d6
Ben <bigriffin94@outlook.com>
2019-10-14 16:08:01 -0700

    Testing Headers are working properly

--- routers/user-router.js
+++ routers/user-router.js
@@ -51,20 +51,24 @@ router.get('/users', protected, (req, res) => {
 // Middleware protection
 function protected(req, res, next) {
 
-    let { username, password } = req.body;
+    let { username, password } = req.headers;
 
-    Users.findBy({ username })
+    if(username && password) {
+        Users.findBy({ username })
         .first()
         .then(user => {
             if(user && bcrypt.compareSync(password, user.password)) {
-            next();
+                next();
             } else {
-                res.status(401).json({ message: "You shall not pass!" })
+                res.status(401).json({ message: "Invalid Credentials" });
             }
         })
         .catch(err => {
-            res.status(500).json({ message: "There was an error logging in." })
+            res.status(500).json(err)
         })
+    } else {
+        res.status(400).json({ message: 'Please provide valid credentials' })
+    }
 };
 
 

23fb4c9f00c6470b597fcf04616a7897031b4223
Ben <bigriffin94@outlook.com>
2019-10-14 14:17:49 -0700

    Migration 1 users table added with id, username, password columns

---
+++ migrations/20191014141340_users_bootstrap.js
@@ -0,0 +1,18 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('users', tbl => {
+      tbl.increments()
+        .notNullable();
+    
+    tbl.string('username', 128)
+        .notNullable()
+        .unique();
+
+    tbl.string('password', 128)
+        .notNullable();
+  })
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('users')
+};
